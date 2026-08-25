75f5c8d43c0245d9880f4e1c2f768358c8d78b13
Deep Patel <DP1852001@GMAIL.COM>
2026-08-25 21:25:52 +0200

    Update import path for projectsData

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { repositoryGroups } from 'src/data/projectsData.js';
+import { repositoryGroups } from '../data/projectsData';
 
 gsap.registerPlugin(ScrollTrigger);
 
